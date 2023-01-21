ddec3a40c0268bb4231c4a9d1fab13eb5818b185
Cristiii <cristian.vlad10@gmail.com>
2023-01-21 13:06:04 +0200

    locked the endpoints

---
+++ bankApp/src/main/java/com/example/bankapp/config/SecurityConfig.java
@@ -0,0 +1,20 @@
+package com.example.bankapp.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests()
+                .requestMatchers("/myAccount","/myBalance","/myLoans","/myCards").authenticated()
+                .requestMatchers("/contact","/notices").permitAll();
+        http.formLogin();
+        http.httpBasic();
+        return http.build();
+    }
+}
