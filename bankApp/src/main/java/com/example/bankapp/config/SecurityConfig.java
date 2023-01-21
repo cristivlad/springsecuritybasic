015cca9f0f85bc73c5117e2dc51942a1d67f141a
Cristiii <cristian.vlad10@gmail.com>
2023-01-21 20:44:10 +0200

    added userDetails bean

--- bankApp/src/main/java/com/example/bankapp/config/SecurityConfig.java
+++ bankApp/src/main/java/com/example/bankapp/config/SecurityConfig.java
@@ -3,6 +3,11 @@ package com.example.bankapp.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -16,5 +21,26 @@ public class SecurityConfig {
         http.formLogin();
         http.httpBasic();
         return http.build();
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+        UserDetails admin = User
+                .withUsername("admin")
+                .password("admin")
+                .authorities("admin")
+                .build();
+        UserDetails user = User
+                .withUsername("user")
+                .password("user")
+                .authorities("read")
+                .build();
+
+        return new InMemoryUserDetailsManager(admin, user);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
     }
 }
