1af2e0686a47b187b3f63e0ff2967467774577bb
Cristiii <cristian.vlad10@gmail.com>
2023-01-22 13:56:02 +0200

    added bcrypt pwd encoder to register

--- bankApp/src/main/java/com/example/bankapp/controller/LoginController.java
+++ bankApp/src/main/java/com/example/bankapp/controller/LoginController.java
@@ -5,6 +5,7 @@ import com.example.bankapp.repository.CustomerRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,10 +15,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class LoginController {
 
     private final CustomerRepository customerRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @PostMapping("/register")
     public ResponseEntity<String> registerUser(@RequestBody Customer customer) {
         try {
+            String hashPwd = passwordEncoder.encode(customer.getPwd());
+            customer.setPwd(hashPwd);
             var savedCustomer = customerRepository.save(customer);
             if (savedCustomer.getId() > 0) {
                 return ResponseEntity.status(HttpStatus.CREATED).body("Given user details are successfully registered");
