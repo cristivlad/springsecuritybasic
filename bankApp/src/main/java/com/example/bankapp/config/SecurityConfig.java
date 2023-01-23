3d81456161c74db9cc4aa6f45e949d15bd340e6d
Cristiii <cristian.vlad10@gmail.com>
2023-01-23 12:58:21 +0200

    added csrf request filter

--- bankApp/src/main/java/com/example/bankapp/config/SecurityConfig.java
+++ bankApp/src/main/java/com/example/bankapp/config/SecurityConfig.java
@@ -1,11 +1,18 @@
 package com.example.bankapp.config;
 
+import com.example.bankapp.filter.CsrfCookieFilter;
+import org.apache.catalina.filters.CsrfPreventionFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.csrf.CsrfFilter;
+import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
+import org.springframework.security.web.server.csrf.CsrfWebFilter;
 import org.springframework.web.cors.CorsConfiguration;
 
 import static java.util.List.of;
@@ -15,6 +22,8 @@ public class SecurityConfig {
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        CsrfTokenRequestAttributeHandler requestHandler = new CsrfTokenRequestAttributeHandler();
+        requestHandler.setCsrfRequestAttributeName("_csrf");
         http
                 .cors().configurationSource(request -> {
                     CorsConfiguration config = new CorsConfiguration();
@@ -25,10 +34,11 @@ public class SecurityConfig {
                     config.setMaxAge(3600L);
                     return config;
                 }).and()
-                .csrf().disable()
+                .csrf(csrf -> csrf.csrfTokenRequestHandler(requestHandler).ignoringRequestMatchers("/contact", "/register").csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()))
+                .addFilterAfter(new CsrfCookieFilter(), BasicAuthenticationFilter.class)
                 .authorizeHttpRequests()
-                .requestMatchers("/myAccount","/myBalance","/myLoans","/myCards","/user").authenticated()
-                .requestMatchers("/contact","/notices", "/register").permitAll();
+                .requestMatchers("/myAccount", "/myBalance", "/myLoans", "/myCards", "/user").authenticated()
+                .requestMatchers("/contact", "/notices", "/register").permitAll();
         http.formLogin();
         http.httpBasic();
         return http.build();
